f972f7215ef19182a424d1cf1970372455ca1b4d
dmitriy.belgiets <chegidim@gmail.com>
2017-05-22 20:45:47 +0300

    updated init algorithm

--- js/overlay-popup.js
+++ js/overlay-popup.js
@@ -4,6 +4,7 @@
   class Popup {
     constructor(options, jqueryObject) {
       this.options = $.extend({
+        callBtn: false,
         beforeShow: function() {},
         beforeHide: function() {},
         afterInit: function() {},
@@ -39,65 +40,73 @@
     }
 
     initPopup() {
-      let popup = this,
-        overlay = this.overlay,
-        srcHtml = overlay.html();
+      try {
+        let popup = this,
+          overlay = this.overlay,
+          srcHtml = overlay.html();
 
-      /* create html */
-      overlay.addClass(this.initClass).attr(this.dataAttr, this.id);
-      overlay.html('<div class="' + this.wrClass + '"><div class="' + this.contentClass + ' ' +
-        'op-' + this.options.position + '">' + srcHtml + '</div>');
+        /* create html */
+        overlay.addClass(this.initClass).attr(this.dataAttr, this.id);
+        overlay.html('<div class="' + this.wrClass + '"><div class="' + this.contentClass + ' ' +
+          'op-' + this.options.position + '">' + srcHtml + '</div>');
 
-      /* apply style options */
-      $('.' + this.initClass).css({backgroundColor: this.options.overlayColor});
-      $('.' + this.contentClass).css({
-        backgroundColor: this.options.popupColor,
-        width: this.options.width,
-        height: this.options.height
-      });
-
-      /* bind events */
-      $(document).on(this.eventShow, function () {
-        /* before show callback */
-        if (popup.options.beforeShow && typeof popup.options.beforeShow === "function") {
-          popup.options.beforeShow();
-        }
-
-        overlay.addClass(popup.switchClass);
-        $("body").css("overflow", "hidden");
-      });
-
-      $(document).on(this.eventHide, function () {
-        /* before hide callback */
-        if (popup.options.beforeHide && typeof popup.options.beforeHide === "function") {
-          popup.options.beforeHide();
-        }
-
-        overlay.removeClass(popup.switchClass);
-        $("body").css("overflow", "visible");
-      });
-
-      /* close overlay popup button */
-      if (this.options.closeBtn) {
-        this.btnObj = $('<div class="op-close"><div class="op-close-in"></div></div>');
-
-        if ('default' === this.options.closeBtn) {
-          $('.op-content', overlay).prepend(this.btnObj);
-        } else {
-          this.btnObj = $(this.options.closeBtn);
-        }
-
-        this.btnObj.bind(this.closeClick, function () {
-          $(document).trigger(popup.eventHide);
+        /* apply style options */
+        $('.' + this.initClass).css({backgroundColor: this.options.overlayColor});
+        $('.' + this.contentClass).css({
+          backgroundColor: this.options.popupColor,
+          width: this.options.width,
+          height: this.options.height
         });
-      }
 
-      /* save data */
-      this.overlay.data(dataKey, this);
+        /* bind events */
+        $(document).on(this.eventShow, function () {
+          /* before show callback */
+          if (popup.options.beforeShow && typeof popup.options.beforeShow === "function") {
+            popup.options.beforeShow();
+          }
 
-      /* after init callback */
-      if (popup.options.afterInit && typeof popup.options.afterInit === "function") {
-        popup.options.afterInit(overlay);
+          overlay.addClass(popup.switchClass);
+          $("body").css("overflow", "hidden");
+        });
+
+        $(document).on(this.eventHide, function () {
+          /* before hide callback */
+          if (popup.options.beforeHide && typeof popup.options.beforeHide === "function") {
+            popup.options.beforeHide();
+          }
+
+          overlay.removeClass(popup.switchClass);
+          $("body").css("overflow", "visible");
+        });
+
+        popup.options.callBtn.on('click', function() {
+          $(document).trigger(popup.eventShow);
+        });
+
+        /* close overlay popup button */
+        if (this.options.closeBtn) {
+          this.btnObj = $('<div class="op-close"><div class="op-close-in"></div></div>');
+
+          if ('default' === this.options.closeBtn) {
+            $('.op-content', overlay).prepend(this.btnObj);
+          } else {
+            this.btnObj = $(this.options.closeBtn);
+          }
+
+          this.btnObj.bind(this.closeClick, function () {
+            $(document).trigger(popup.eventHide);
+          });
+        }
+
+        /* save data */
+        this.overlay.data(dataKey, this);
+
+        /* after init callback */
+        if (popup.options.afterInit && typeof popup.options.afterInit === "function") {
+          popup.options.afterInit(overlay);
+        }
+      } catch(e) {
+        console.log(e);
       }
     }
 
